89dc7baa78cdfad9e8fd60d009c1acfc8ddbfa80
OxygenO22 <bamaik@mail.ru>
2024-09-05 22:45:51 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { Login } from './features/Login/Login';
+import { TodolistsList } from './features/TodolistsList/TodolistsList';
+import { ErrorPage } from './components/ErrorPage/ErrorPage';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Navigate to="404" />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/todolists" />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/todolists",
+        element: <TodolistsList />,
+      },
+    ],
+  },
+  {
+    path: "404",
+    element: <ErrorPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
